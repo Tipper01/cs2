7d4761c889638285ec55dfcf3a62bce5e83a00aa
Tipper01 <kyletippets31@gmail.com>
2017-10-13 10:18:39 -0600

    A third commit

--- GitTester/src/GitTest.java
+++ GitTester/src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("A Git Test");
 		System.out.println("Another change.");
+		System.out.println("Yet another change.");
 	}
 
 }
